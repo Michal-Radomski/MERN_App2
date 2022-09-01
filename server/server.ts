201975683f3898623bc74d80b6729a37f7c97aa6
Michal Radomski <michalra@op.pl>
2022-09-01 18:16:02 +0200

    MongoDB was connected

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,40 @@
 const express = require("express");
 import {Request, Response} from "express";
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// App
 const app = express();
 
+// Mongo DB
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con: {connection: {host: string}}) => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+  })
+  .catch((error: string) => console.log("Mongo DB Error => ", error));
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// Route
 app.get("/", (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+  //   res.json({
+  //     data: 'You Reached Nodejs API For React-Node CRUD App'
+  // });
+  res.send("<h1 style='color:darkblue;text-align:center'>API is running</h1>");
 });
 
+// Port
 const port = (process.env.PORT || 5000) as number;
 
 app.listen({port: port}, () => {
